ddac3a0687b0b2fb4ad4295e918894d5976fd4c2
Arnold Martinez <arnoldmartinez@criptext.com>
2022-05-09 17:37:56 -0500

    Access microservices via API gateway
    Cap. 53

--- src/main/java/com/appsdeveloperblog/photoapp/api/gateway/PhotoAppApiZuulApiApplication.java
+++ src/main/java/com/appsdeveloperblog/photoapp/api/gateway/PhotoAppApiZuulApiApplication.java
@@ -2,10 +2,14 @@ package com.appsdeveloperblog.photoapp.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
+//@EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class PhotoAppApiZuulApiApplication {
